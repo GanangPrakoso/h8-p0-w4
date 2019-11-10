7b1ed192d9f91f9cfb07ba0412a8e3c09669d723
GanangPrakoso <ganangmanggolo@gmail.com>
2019-11-10 22:08:37 +0700

    revisiBos

--- exercise11_shoppingTime.js
+++ exercise11_shoppingTime.js
@@ -1,74 +1,55 @@
 function shoppingTime(memberId, money) {
-    var shopping = {}
-
-    if (memberId === '' || money !== Number(money)){
+  if (memberId===''){
       return 'Mohon maaf, toko X hanya berlaku untuk member saja'
-    }
-    
-    var stacattu = 1500000;
-    var zoro = 500000;
-    var hnm = 250000;
-    var uniklooh = 175000;
-    var casingHandphone = 50000;
-    var change = money;
-
-    shopping['memberId'] = memberId;    
-    shopping['money'] = money;
-
-    if (money<casingHandphone){
-      return 'Mohon maaf, uang tidak cukup'
-    }
-
-    if (money < uniklooh && money >= casingHandphone){
-      shopping['listPurchased'] = ['Casing Handphone']
-      change-=casingHandphone
-      shopping['changeMoney'] = change;
-      return shopping;
-    }
-    if (money < hnm && money >= uniklooh){
-      shopping['listPurchased'] = ['Sweater Uniklooh', 'Casing Handphone']
-      chenge-=uniklooh-casingHandphone;
-      shopping['changeMoney'] = change;
-      return shopping;
-    }
-    if (money < zoro && money >= hnm){
-      shopping['listPurchased'] = ['Baju H&N', 'Sweater Uniklooh', 'Casing Handphone']
-      change-=hnm-uniklooh-casingHandphone;
-      shopping['changeMoney'] = change;
-      return shopping
-    }
-    if (money <stacattu && money>= zoro){
-      shopping['listPurchased'] = ['Baju Zoro', 'Baju H&N', 'Sweater Uniklooh', 'Casing Handphone'];
-      change-=zoro-hnm-uniklooh-casingHandphone;
-      shopping['changeMoney'] = change;
-      return shopping
-    }
-    else {
-      shopping['listPurchased'] = ['Sepatu Stacattu', 'Baju Zoro', 'Baju H&N', 'Sweater Uniklooh', 'Casing Handphone']
-      change = money -  stacattu-zoro-hnm-uniklooh-casingHandphone;
-      shopping['changeMoney'] = change;
-      return shopping;
-    }
-
   }
+  if (money===undefined){
+      return 'Mohon maaf, toko X hanya berlaku untuk member saja'
+  }
+  if (money<50000){
+      return 'Mohon maaf, uang tidak cukup'
+  }
+
+  let hedonism = {};
+  let belonjo = [];
+  const barang = [['Sepatu Stacattu', 1500000],['Baju Zoro', 500000],['Baju H&N', 250000],['Sweater Uniklooh', 175000],['Casing Handphone', 50000]]
+
+  let sisaUang = money;
+
+  hedonism.memberId = memberId;
+  hedonism.money = money;
   
-  // TEST CASES
-  console.log(shoppingTime('1820RzKrnWn08', 2475000));
-    //{ memberId: '1820RzKrnWn08',
-    // money: 2475000,
-    // listPurchased:
-    //  [ 'Sepatu Stacattu',
-    //    'Baju Zoro',
-    //    'Baju H&N',
-    //    'Sweater Uniklooh',
-    //    'Casing Handphone' ],
-    // changeMoney: 0 }
-  console.log(shoppingTime('82Ku8Ma742', 170000));
-  //{ memberId: '82Ku8Ma742',
-  // money: 170000,
+  for (let i=0; i<=barang.length-1; i++){
+      if (sisaUang>=barang[i][1]){
+          belonjo.push(barang[i][0]);
+          sisaUang-=barang[i][1]
+      }
+      // if (sisaUang<50000){
+      //     break;
+      // }
+  }
+  hedonism.listPurchased = belonjo;
+  hedonism.changeMoney = sisaUang;
+
+  return hedonism;
+}
+
+// TEST CASES
+console.log(shoppingTime('1820RzKrnWn08', 2475000));
+  //{ memberId: '1820RzKrnWn08',
+  // money: 2475000,
   // listPurchased:
-  //  [ 'Casing Handphone' ],
-  // changeMoney: 120000 }
-  console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
-  console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
-  console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
+  //  [ 'Sepatu Stacattu',
+  //    'Baju Zoro',
+  //    'Baju H&N',
+  //    'Sweater Uniklooh',
+  //    'Casing Handphone' ],
+  // changeMoney: 0 }
+console.log(shoppingTime('82Ku8Ma742', 170000));
+//{ memberId: '82Ku8Ma742',
+// money: 170000,
+// listPurchased:
+//  [ 'Casing Handphone' ],
+// changeMoney: 120000 }
+console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
+console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
+console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
